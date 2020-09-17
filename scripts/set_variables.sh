49c023cf4307e4555ab21e79db2250c1d59c1644
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-17 18:02:04 +0100

    Update set_variables.sh

--- scripts/set_variables.sh
+++ scripts/set_variables.sh
@@ -14,9 +14,9 @@ DOT_PORT=853
 
 ### Dynamically determine OpenVPN port and protocol ###
 echo '[info] Determine openvpn port from config file'
-OPENVPN_PORT=$(grep -m 1 "remote " /config/openvpn/openvpn.ovpn) ; OPENVPN_PORT="$(echo $OPENVPN_PORT | sed 's/ /   /g')" ; OPENVPN_PORT=${OPENVPN_PORT:(-5)} ; OPENVPN_PORT="$(echo $OPENVPN_PORT | sed 's/ //g')" ; OPENVPN_PORT="$(echo $OPENVPN_PORT | sed 's/[^0-9]*//g')"
+OPENVPN_PORT=$(grep -m 1 "remote " /root/openvpn/openvpn.ovpn) ; OPENVPN_PORT="$(echo $OPENVPN_PORT | sed 's/ /   /g')" ; OPENVPN_PORT=${OPENVPN_PORT:(-5)} ; OPENVPN_PORT="$(echo $OPENVPN_PORT | sed 's/ //g')" ; OPENVPN_PORT="$(echo $OPENVPN_PORT | sed 's/[^0-9]*//g')"
 echo '[info] Determine openvpn protocol from config file'
-OPENVPN_PROTO=$(grep -m 1 "proto " /config/openvpn/openvpn.ovpn) ; OPENVPN_PROTO="$(echo $OPENVPN_PROTO | sed 's/proto //g')"
+OPENVPN_PROTO=$(grep -m 1 "proto " /root/openvpn/openvpn.ovpn) ; OPENVPN_PROTO="$(echo $OPENVPN_PROTO | sed 's/proto //g')"
 echo "[info] Will connect openvpn on port=$OPENVPN_PORT proto=$OPENVPN_PROTO"
 
 ### Dynamically determine eth0 network for iptables ###
