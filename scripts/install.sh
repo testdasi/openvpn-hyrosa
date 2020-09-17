dfe6e54313f07a4c7ebf0f6073af13f071867be5
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-17 16:16:06 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-# install deluge and deluge-web
-apt-get -y update \
-    && apt-get -y install deluged deluge-web
-
-# clean up
-apt-get -y autoremove \
-    && apt-get -y autoclean \
-    && apt-get -y clean \
-    && rm -fr /tmp/* /var/tmp/* /var/lib/apt/lists/*
-
 # chmod scripts
 chmod +x /*.sh
