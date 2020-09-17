e7dc23e8ea8f5383c20c4f919ab098e718f434d2
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-17 16:22:13 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -15,7 +15,7 @@ echo '[info] All configs fixed'
 ### Stubby DNS-over-TLS ###
 echo ''
 echo "[info] Run stubby in background on port $DNS_PORT"
-stubby -g -C /config/stubby/stubby.yml
+stubby -g -C /root/stubby/stubby.yml
 ipnaked=$(dig +short myip.opendns.com @208.67.222.222)
 echo "[warn] Your ISP public IP is $ipnaked"
 
@@ -39,28 +39,28 @@ echo '[info] Done'
 ### Dante SOCKS proxy to VPN ###
 echo ''
 echo "[info] Run danted in background on port $DANTE_PORT"
-danted -D -f /config/dante/danted.conf
+danted -D -f /root/dante/danted.conf
 
 ### Tinyproxy HTTP proxy to VPN ###
 echo ''
 echo "[info] Run tinyproxy in background with no log on port $TINYPROXY_PORT"
-tinyproxy -c /config/tinyproxy/tinyproxy.conf
+tinyproxy -c /root/tinyproxy/tinyproxy.conf
 
 ### sabnzbdplus
 echo ''
 echo "[info] Run sabnzbdplus in background on HTTP port $SAB_PORT_A and HTTPS port $SAB_PORT_B"
-sabnzbdplus --daemon --config-file /config/sabnzbdplus/sabnzbdplus.ini --pidfile /config/sabnzbdplus/sabnzbd.pid
+sabnzbdplus --daemon --config-file /root/sabnzbdplus/sabnzbdplus.ini --pidfile /root/sabnzbdplus/sabnzbd.pid
 
 ### deluge
 echo ''
 echo "[info] Run deluge in background on HTTP port $DELUGE_PORT"
-deluged --quiet --port=58846 --config=/config/deluge-web
-deluge-web --fork --quiet --config=/config/deluge-web
+deluged --quiet --port=58846 --config=/root/deluge-web
+deluge-web --fork --quiet --config=/root/deluge-web
 
 ### nzbhydra2
 echo ''
 echo "[info] Run nzbhydra2 in background on port $HYDRA_PORT"
-/app/nzbhydra2/nzbhydra2 --daemon --nobrowser --java /usr/lib/jvm/java-11-openjdk-amd64/bin/java --datafolder /config/nzbhydra2 --pidfile /config/nzbhydra2/nzbhydra2.pid
+/app/nzbhydra2/nzbhydra2 --daemon --nobrowser --java /usr/lib/jvm/java-11-openjdk-amd64/bin/java --datafolder /root/nzbhydra2 --pidfile /root/nzbhydra2/nzbhydra2.pid
 
 ### Infinite loop to stop docker from stopping ###
 sleep 10s
@@ -77,11 +77,11 @@ do
     echo "[info] danted PID: $pidlist"
     pidlist=$(pidof tinyproxy)
     echo "[info] tinyproxy PID: $pidlist"
-    pidlist=$(cat /config/sabnzbdplus/sabnzbd.pid)
+    pidlist=$(cat /root/sabnzbdplus/sabnzbd.pid)
     echo "[info] sabnzbdplus PID: $pidlist"
-    pidlist=$(cat /config/deluge-web/deluged.pid | sed 's/;58846//g')
+    pidlist=$(cat /root/deluge-web/deluged.pid | sed 's/;58846//g')
     echo "[info] deluge PID: $pidlist"
-    pidlist=$(cat /config/nzbhydra2/nzbhydra2.pid)
+    pidlist=$(cat /root/nzbhydra2/nzbhydra2.pid)
     echo "[info] nzbhydra2 PID: $pidlist"
     sleep 3600s
 done
