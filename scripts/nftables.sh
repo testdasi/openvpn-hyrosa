df011bc5c94244acaedb8f1a85481fd617d79def
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-17 16:12:08 +0100

    Create nftables.sh

---
+++ scripts/nftables.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+### Flusing ruleset and add missing route ###
+echo '[info] Flusing ruleset'
+nft flush ruleset
+add_route="$(ip route | grep 'default')" ; add_route="$(sed "s|default|$HOST_NETWORK|g" <<< $add_route)"
+ip route add $add_route
+echo "[info] Added route $add_route"
+
+### Editing ruleset ###
+echo '[info] Editing base ruleset'
+rm -f /nftables.rules
+cp /ruleset.nft /nftables.rules
+sed -i "s|_ETH0_NET_|$ETH0_NET|g" '/nftables.rules'
+sed -i "s|_HOST_NETWORK_|${HOST_NETWORK}|g" '/nftables.rules'
+sed -i "s|_OPENVPN_PROTO_|$OPENVPN_PROTO|g" '/nftables.rules'
+sed -i "s|_OPENVPN_PORT_|$OPENVPN_PORT|g" '/nftables.rules'
+sed -i "s|_DNS_PORT_|$DNS_PORT|g" '/nftables.rules'
+sed -i "s|_DANTE_PORT_|$DANTE_PORT|g" '/nftables.rules'
+sed -i "s|_TINYPROXY_PORT_|$TINYPROXY_PORT|g" '/nftables.rules'
+sed -i "s|_SAB_PORT_A_|$SAB_PORT_A|g" '/nftables.rules'
+sed -i "s|_SAB_PORT_B_|$SAB_PORT_B|g" '/nftables.rules'
+sed -i "s|_HYDRA_PORT_|$HYDRA_PORT|g" '/nftables.rules'
+sed -i "s|_DELUGE_PORT_|$DELUGE_PORT|g" '/nftables.rules'
+
+### Add rules ###
+echo '[info] Apply rules'
+nft -f /nftables.rules
+rm /nftables.rules
