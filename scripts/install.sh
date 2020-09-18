86a9175a8fffc6d5d9a0cd7f58f964f6f037598c
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 13:17:05 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# install static files
+mkdir -p /temp \
+    && cd /temp \
+    && curl -L "https://github.com/testdasi/static/archive/master.zip" -o /temp/static.zip \
+    && unzip /temp/static.zip \
+    && rm -f /temp/static.zip \
+    && mv /temp/static-master /static
+
 # chmod scripts
 chmod +x /*.sh
