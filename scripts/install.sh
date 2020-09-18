e6fd88afafde42b52b0387945dc390b909fab582
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 20:11:14 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,13 +16,13 @@ cp -f /temp/* /static/config/ \
 sed -i "s|\/data\/deluge\/watch|\/data\/rtorrent\/watch|g" '/static/config/nzbhydra.yml'
 sed -i "s|\/config\/openvpn|\/root\/openvpn|g" '/static/scripts/openvpn.sh'
 sed -i "s|\/config\/openvpn|\/root\/openvpn|g" '/static/scripts/set_variables_ovpn_port_proto.sh'
-sed -i "s|/config/|\/root\/|g" '/static/scripts/fix_config_stubby.sh'
-sed -i "s|/config/|\/root\/|g" '/static/scripts/fix_config_dante.sh'
-sed -i "s|/config/|\/root\/|g" '/static/scripts/fix_config_tinyproxy.sh'
-sed -i "s|/config/|\/root\/|g" '/static/scripts/fix_config_sabnzbdplus.sh'
-sed -i "s|/config/|\/root\/|g" '/static/scripts/fix_config_rtorrent.sh'
-sed -i "s|/config/|\/root\/|g" '/static/scripts/fix_config_flood.sh'
-sed -i "s|/config/|\/root\/|g" '/static/scripts/fix_config_nzbhydra2.sh'
+sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_stubby.sh'
+sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_dante.sh'
+sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_tinyproxy.sh'
+sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_sabnzbdplus.sh'
+sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_rtorrent.sh'
+sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_flood.sh'
+sed -i "s|\/etc\/|\/root\/|g" '/static/scripts/fix_config_nzbhydra2.sh'
 
 # chmod scripts
 chmod +x /*.sh
