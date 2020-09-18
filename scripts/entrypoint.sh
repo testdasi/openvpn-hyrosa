e6d61f675b74d49176e3dac09afcea093e980037
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 13:38:57 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -25,11 +25,6 @@ echo '[info] Set up nftables rules'
 source /nftables.sh
 echo '[info] All rules created'
 
-### Quick block test ####
-echo ''
-ipttest=$(dig +short +time=5 +tries=1 myip.opendns.com @208.67.222.222)
-echo "[info] Quick block test. Expected result is time out. Actual result is $ipttest"
-
 ### OpenVPN ###
 echo ''
 echo "[info] Setting up OpenVPN tunnel"
