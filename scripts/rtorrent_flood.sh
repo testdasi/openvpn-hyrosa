453bf3188f9bfcf3493b3ba8f5206ce6028aa606
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-18 22:50:43 +0100

    Create rtorrent_flood.sh

---
+++ scripts/rtorrent_flood.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while true
+do
+    rtorrent_pidlist=$(pidof /usr/bin/rtorrent)
+    if [ -z "$rtorrent_pidlist" ]
+    then
+        screen -d -m -fa -S rtorrent /usr/bin/rtorrent
+    fi
+    
+    flood_pidlist=$(pidof npm)
+    if [ -z "$flood_pidlist" ]
+    then
+        cd /app/flood \
+        && screen -d -m -fa -S flood npm start
+    fi
+    
+    sleep 60s
+done
