08cac4429d7fc15032defea9987ecc9a448e9526
testdasi <51292604+testdasi@users.noreply.github.com>
2020-10-02 14:59:45 +0100

    Split out pid-check.sh and reduce max wait time
    Make it more convenient to manually restart crashed processes

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -78,97 +78,7 @@ then
         iphiden=$(dig +short myip.opendns.com @208.67.222.222)
         echo "[info] Your VPN public IP is $iphiden"
         
-        pidlist=$(pidof stubby)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] stubby crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            stubby -g -C /root/stubby/stubby.yml
-        else
-            echo "[info] stubby PID: $pidlist"
-        fi
-        
-        pidlist=$(pidof openvpn)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] openvpn crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            source /static/scripts/openvpn.sh
-        else
-            echo "[info] openvpn PID: $pidlist"
-        fi
-        
-        pidlist=$(pidof danted)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] danted crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            danted -D -f /root/dante/danted.conf
-        else
-            echo "[info] danted PID: $pidlist"
-        fi
-        
-        pidlist=$(pidof tinyproxy)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] tinyproxy crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            tinyproxy -c /root/tinyproxy/tinyproxy.conf
-        else
-            echo "[info] tinyproxy PID: $pidlist"
-        fi
-        
-        #pidlist=$(cat /root/sabnzbdplus/sabnzbd.pid)
-        pidlist=$(pidof python2)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] sabnzbdplus crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            sabnzbdplus --daemon --config-file /root/sabnzbdplus/sabnzbdplus.ini --pidfile /root/sabnzbdplus/sabnzbd.pid
-        else
-            echo "[info] sabnzbdplus PID: $pidlist"
-        fi
-        
-        pidlist=$(pidof rtorrent)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] rtorrent crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            screen -d -m -fa -S rtorrent /usr/bin/rtorrent
-        else
-            echo "[info] rtorrent PID: $pidlist"
-        fi
-        
-        pidlist=$(pidof npm)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] flood crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            start-stop-daemon --start --background --name flood --chdir /app/flood --exec /app/flood/flood.sh
-        else
-            echo "[info] flood PID: $pidlist"
-        fi
-        
-        #pidlist=$(cat /root/nzbhydra2/nzbhydra2.pid)
-        pidlist=$(pidof nzbhydra2)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] nzbhydra2 crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            /app/nzbhydra2/nzbhydra2 --daemon --nobrowser --java /usr/lib/jvm/java-11-openjdk-amd64/bin/java --datafolder /root/nzbhydra2 --pidfile /root/nzbhydra2/nzbhydra2.pid
-        else
-            echo "[info] nzbhydra2 PID: $pidlist"
-        fi
-        
-        pidlist=$(pidof python3)
-        if [ -z "$pidlist" ]
-        then
-            echo '[warn] WebUI launcher crashed, restarting'
-            crashed=$(( $crashed + 1 ))
-            start-stop-daemon --start --background --name launcher --chdir /app/launcher --exec /app/launcher/launcher-python3.sh
-        else
-            echo "[info] WebUI launcher PID: $pidlist"
-        fi
+        source /static/scripts/pid-check.sh
         
         # reset wait time if something crashed, otherwise double the wait time till next healthcheck
         if (( $crashed > 0 ))
@@ -178,9 +88,9 @@ then
         else
             sleep_time=$(( $sleep_time * 2 ))
             # restrict wait time to within 3600s i.e. 1hr
-            if (( $sleep_time > 3600 ))
+            if (( $sleep_time > 360 ))
             then
-                sleep_time=3600
+                sleep_time=360
             fi
         fi
     done
